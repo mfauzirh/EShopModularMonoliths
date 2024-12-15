c2408ba4e632f6bb8ba0284daf98f71226581091
mfauzirh <mfauzirizkih@gmail.com>
2024-12-15 17:20:50 +0700

    modify product to rich domain model

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,53 @@
+namespace Catalog.Products.Models;
+
+public class Product : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public List<string> Category { get; private set; } = [];
+    public string Description { get; private set; } = default!;
+    public string ImageFile { get; private set; } = default!;
+    public decimal Price { get; private set; }
+
+    public static Product Create(
+        Guid id,
+        string name,
+        List<string> category,
+        string description,
+        string imageFile,
+        decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            Description = description,
+            ImageFile = imageFile,
+            Price = price
+        };
+
+        return product;
+    }
+
+    public void Update(
+        string name,
+        List<string> category,
+        string description,
+        string imageFile,
+        decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        Name = name;
+        Category = category;
+        Description = description;
+        ImageFile = imageFile;
+        Price = price;
+
+        // TODO: if price change, raise ProductPriceChanged domain event
+    }
+}
