19ead53f558418aca6c5cbad0ffd1a3bb0b8acb9
mfauzirh <mfauzirizkih@gmail.com>
2024-12-17 13:20:37 +0700

    add domain event dispatcher interceptors

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shared.Data.Interceptors;
@@ -10,11 +11,20 @@ public static class CatalogModule
     public static IServiceCollection AddCatalogModule(
         this IServiceCollection services, IConfiguration configuration) 
     {
+        // Application use case service
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
         // Data - Infrastructure services
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
         var connectionString = configuration.GetConnectionString("Database");
 
-        services.AddDbContext<CatalogDbContext>(options => {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+        services.AddDbContext<CatalogDbContext>((serviceProvider, options) => {
+            options.AddInterceptors(serviceProvider.GetServices<ISaveChangesInterceptor>());
             options.UseNpgsql(connectionString);
         });
 
