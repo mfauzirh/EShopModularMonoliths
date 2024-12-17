7fcf6ce10a92679ea0ceab59e37125666894f7fd
mfauzirh <mfauzirizkih@gmail.com>
2024-12-17 10:26:37 +0700

    add auto migration on startup

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -21,6 +21,15 @@ public static class CatalogModule
     public static IApplicationBuilder UseCatalogModule(
         this IApplicationBuilder app)
     {
+        InitializeDatabaseAsync(app).GetAwaiter().GetResult();
+
         return app;
+    }
+
+    private static async Task InitializeDatabaseAsync(IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+        await context.Database.MigrateAsync();
     }
 }
