33a69f2a25901ce9b7d640ace36d14c53830a8d4
mfauzirh <mfauzirizkih@gmail.com>
2024-12-17 10:38:04 +0700

    refactor generic use migrations

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
 
 namespace Catalog;
 
@@ -21,15 +22,9 @@ public static class CatalogModule
     public static IApplicationBuilder UseCatalogModule(
         this IApplicationBuilder app)
     {
-        InitializeDatabaseAsync(app).GetAwaiter().GetResult();
+        // Use Data - Infrastructure services
+        app.UseMigration<CatalogDbContext>();
 
         return app;
-    }
-
-    private static async Task InitializeDatabaseAsync(IApplicationBuilder app)
-    {
-        using var scope = app.ApplicationServices.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
-        await context.Database.MigrateAsync();
     }
 }
