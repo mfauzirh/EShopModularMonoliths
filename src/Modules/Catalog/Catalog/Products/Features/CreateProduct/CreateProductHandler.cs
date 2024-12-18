711d118077aa9b36d7f07661a3370db5808afbc0
mfauzirh <mfauzirizkih@gmail.com>
2024-12-18 10:03:04 +0700

    refactor handler

--- src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,14 @@
+using Catalog.Products.Dtos;
 using MediatR;
+using Shared.CQRS;
 
 namespace Catalog.Products.Features.CreateProduct;
 
-public record CreateProductCommand(
-    string Name,
-    List<string> Category,
-    string Description,
-    string ImageFile,
-    decimal Price
-) : IRequest<CreateProductResult>;
+public record CreateProductCommand(ProductDto Product) : ICommand<CreateProductResult>;
 
-public record CreateProductResult(Guid id);
+public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
